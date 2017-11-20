d3b8ade0a6029811b3238a6bd1766ffad8166b0f
Edwards Presume <edwardspresume@aol.com>
2017-11-19 22:58:18 -0500

    js practice

--- Array Search/script.js
+++ Array Search/script.js
@@ -1,2 +1,8 @@
 //An array of our store items
-let items = ['apple', 'cake', 'water', 'bat', 'computer'];
+let items = ['apple', 'orange', 'water', 'Blue berries'];
+
+//Print function
+let print = msg => {
+  let output = document.getElementById('output');
+  out.innerHTML = `<h1> ${msg} </h1>`;
+};
