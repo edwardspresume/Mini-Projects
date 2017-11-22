496190d402e35345ef576945ebfd25b82f1589bd
Edwards Presume <edwardspresume@aol.com>
2017-11-21 22:31:07 -0500

    Memory practice

--- Array Search/script.js
+++ Array Search/script.js
@@ -1,8 +1,12 @@
-//An array of our store items
-let items = ['apple', 'orange', 'water', 'Blue berries'];
+//An array of the items in our Store
+let items = ['Blue berries', 'water', 'Avocado', 'Black berries'];
 
 //Print function
 let print = msg => {
   let output = document.getElementById('output');
-  out.innerHTML = `<h1> ${msg} </h1>`;
+  output.innerHTML = `<h1> ${msg} </h1>`;
 };
+
+while (true) {
+  let search = prompt("search up an item in our store. Type 'list' to show all our items ")
+}
