690d1bd210120dd4b8c46c965073ead86b91463f
Edwards Presume <edwardspresume@aol.com>
2017-11-25 23:10:48 -0500

    slight modification

--- js practice/js/helper.js
+++ js practice/js/helper.js
@@ -1,11 +1,23 @@
-//Print's out songs in an ordered list format
+// Print's out songs in an ordered list format
+// let printList = list => {
+//   let htmlList = '<ol>';
+//   for (let i = 0; i < list.length; i++) {
+//     htmlList += '<li>' + list[i] + '</li>';
+//   }
+//
+//   htmlList += '</ol>';
+//   print(htmlList);
+// };
+//
+// let print = html => document.write(html);
+
 let printList = list => {
-  let htmlList = '<ol>';
+  let htmlList = `<ol>`;
   for (let i = 0; i < list.length; i++) {
-    htmlList += '<li>' + list[i] + '</li>';
+    htmlList += `<li> ${list[i]} </li>`;
   }
 
-  htmlList += '</ol>';
+  htmlList += `</ol>`;
   print(htmlList);
 };
 
