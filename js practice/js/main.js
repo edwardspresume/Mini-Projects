d9d2e937b063231dd9234df2102b0f461088bbe5
Edwards Presume <edwardspresume@aol.com>
2017-11-27 22:12:39 -0500

    Refactoring complete

--- js practice/js/main.js
+++ js practice/js/main.js
@@ -1,10 +1,9 @@
 //Array that will contain all the items.
 let items = [];
-let addItems;
 
 //Allows the user to add multiple items till they are done.
 while (true) {
-  addItems = prompt('Type in the songs you wish to enter into your play list. Once your done type "quit" to exit.');
+  let addItems = prompt('Type in the songs you wish to enter into your play list. Once your done type "quit" to exit.');
   if (addItems.toLowerCase() === 'quit') {
     break;
   } else {
