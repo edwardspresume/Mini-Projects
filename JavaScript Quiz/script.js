6a194b1ad5a5a850a2dc133fb3e165eaaacd3548
Edwards Presume <edwardspresume@aol.com>
2017-12-02 23:05:38 -0500

    Running into some error with the refactoring. Will ticker more tomorrow.

--- JavaScript Quiz/script.js
+++ JavaScript Quiz/script.js
@@ -1,5 +1,5 @@
 //Quiz questions and answers
-let quizQA = [
+var quizQA = [
   [
     "How many days are in the week?", 7
   ],
@@ -11,20 +11,37 @@ let quizQA = [
   ]
 ];
 
-//Prompt questions
-let questions;
-
-//Keep track of how many questions are answered correctly
-let score = 0;
-
 //Display which questions where answered correctly and which where not
-let correctQuestions = "<h3> You got these questions correct: </h3> <ol>";
-let incorrectQuestions = "<h3> You got these questions wrong: </h3> <ol>";
+var correctQuestions = `<h3> You got these questions correct: </h3> <ol>`;
+var incorrectQuestions = `<h3> You got these questions wrong: </h3> <ol>`;
 
 //Print the quiz result
-let print = msg => {
+var print = msg => {
   let output = document.getElementById('output');
   output.innerHTML = msg;
 };
 
-print('asd')
+//Sort the questions in list format
+var questionResults = () => `<li> ${quizQA[i][0]} </li>`;
+
+//Loop through the quiz
+for (var i = 0; i < quizQA.length; i++) {
+  //Keep track of how many questions are answered correctly
+  var score = 0;
+
+  //Prompt questions
+  var question = parseInt(prompt(quizQA[i][0]));
+  if (question === quizQA[i][1]) {
+    score++;
+    correctQuestions += questionResults();
+  } else {
+    incorrectQuestions += questionResults();
+  }
+}
+
+correctQuestions += `</ol>`;
+incorrectQuestions += `</ol>`;
+
+print("<p> You got " + score + " question(s) right </p>");
+print(correctQuestions);
+print(incorrectQuestions);
