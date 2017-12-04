6357d3f333000d8207a9ee88037e3c7534d96b0b
Edwards Presume <edwardspresume@aol.com>
2017-12-03 22:48:52 -0500

    Refactoring nearly complete

--- JavaScript Quiz/script.js
+++ JavaScript Quiz/script.js
@@ -1,5 +1,5 @@
 //Quiz questions and answers
-var quizQA = [
+let quizQA = [
   [
     "How many days are in the week?", 7
   ],
@@ -11,26 +11,23 @@ var quizQA = [
   ]
 ];
 
+//Keep track of how many questions are answered correctly
+let score = 0;
+
 //Display which questions where answered correctly and which where not
-var correctQuestions = `<h3> You got these questions correct: </h3> <ol>`;
-var incorrectQuestions = `<h3> You got these questions wrong: </h3> <ol>`;
+let correctQuestions = `<h3> You got these questions correct: </h3> <ol>`;
+let incorrectQuestions = `<h3> You got these questions wrong: </h3> <ol>`;
 
 //Print the quiz result
-var print = msg => {
-  let output = document.getElementById('output');
-  output.innerHTML = msg;
-};
+let print = msg => document.write(msg);
 
 //Sort the questions in list format
-var questionResults = () => `<li> ${quizQA[i][0]} </li>`;
+let questionResults = () => `<li> ${quizQA[i][0]} </li>`;
 
 //Loop through the quiz
 for (var i = 0; i < quizQA.length; i++) {
-  //Keep track of how many questions are answered correctly
-  var score = 0;
-
   //Prompt questions
-  var question = parseInt(prompt(quizQA[i][0]));
+  let question = parseInt(prompt(quizQA[i][0]));
   if (question === quizQA[i][1]) {
     score++;
     correctQuestions += questionResults();
@@ -42,6 +39,6 @@ for (var i = 0; i < quizQA.length; i++) {
 correctQuestions += `</ol>`;
 incorrectQuestions += `</ol>`;
 
-print("<p> You got " + score + " question(s) right </p>");
+print(`<p> You got ${score} question(s) right </p>`);
 print(correctQuestions);
 print(incorrectQuestions);
