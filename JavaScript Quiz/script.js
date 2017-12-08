da521335d8df718e305ea5d980026844cab22037
Edwards Presume <edwardspresume@aol.com>
2017-12-07 19:34:20 -0500

    Enveloped the quiz components inside a function

--- JavaScript Quiz/script.js
+++ JavaScript Quiz/script.js
@@ -11,35 +11,39 @@ let quizQA = [
   ]
 ];
 
-//Keep track of how many questions are answered correctly
-let score = 0;
-
-//Display which questions where answered correctly and which where not
-let correctQuestions = `<h3> You got these questions correct: </h3> <ol>`;
-let incorrectQuestions = `<h3> You got these questions wrong: </h3> <ol>`;
-
-//Sort the questions in list format
-let questionResults = () => `<li> ${quizQA[i][0]} </li>`;
-
 //Print the quiz result
 let print = msg => document.write(msg);
 
-//Loop through the quiz
-for (var i = 0; i < quizQA.length; i++) {
-  //Prompt questions
-  let question = parseInt(prompt(quizQA[i][0]));
+let quizGenerator = () => {
+  //Keep track of how many questions are answered correctly
+  let score = 0;
 
-  if (question === quizQA[i][1]) {
-    score++;
-    correctQuestions += questionResults();
-  } else {
-    incorrectQuestions += questionResults();
+  //Display which questions where answered correctly and which where not
+  let correctQuestions = `<h3> You got these questions correct: </h3> <ol>`;
+  let incorrectQuestions = `<h3> You got these questions wrong: </h3> <ol>`;
+
+  //Sort the questions in list format
+  let questionResults = () => `<li> ${quizQA[i][0]} </li>`;
+
+  //Loop through the quiz
+  for (var i = 0; i < quizQA.length; i++) {
+    //Prompt questions
+    let question = parseInt(prompt(quizQA[i][0]));
+
+    if (question === quizQA[i][1]) {
+      score++;
+      correctQuestions += questionResults();
+    } else {
+      incorrectQuestions += questionResults();
+    }
   }
-}
 
-correctQuestions += `</ol>`;
-incorrectQuestions += `</ol>`;
+  correctQuestions += `</ol>`;
+  incorrectQuestions += `</ol>`;
 
-print(`<p> You got ${score} question(s) right </p>`);
-print(correctQuestions);
-print(incorrectQuestions);
+  print(`<p> You got ${score} question(s) right </p>`);
+  print(correctQuestions);
+  print(incorrectQuestions);
+};
+
+quizGenerator();
