ecadfcce1875377556759ef060fe8fdf5fed865c
Edwards Presume <edwardspresume@aol.com>
2017-12-09 22:24:18 -0500

    Minor changes

--- Student-record-search/js/studentRecord.js
+++ Student-record-search/js/studentRecord.js
@@ -1,44 +1,40 @@
-var student;
-var search;
-var message = '';
+let student;
+let search;
+let message = '';
 
-//Function to print the results 
-var print = (msg) => {
-    var output = document.querySelector("#output");
-    output.innerHTML = msg;
+//Function to print the results
+let print = (msg) => {
+  var output = document.querySelector("#output");
+  output.innerHTML = msg;
 }
-
 
 //Format in which the data will be displayed
 var studentInfo = student => {
-    var report = `<h2> Student: ${student.Name} </h2>`;
+  var report = `<h2> Student: ${student.Name} </h2>`;
 
-    report += `<p> Interest: ${student.Interest} </p>`;
+  report += `<p> Interest: ${student.Interest} </p>`;
 
-    report += `<p> Projects: ${student.projects} </p>`;
+  report += `<p> Projects: ${student.projects} </p>`;
 
-    report += `<p> Years of experience: ${student["Years of experience"]} </p>`;
+  report += `<p> Years of experience: ${student["Years of experience"]} </p>`;
 
-    return report;
+  return report;
 };
-
 
 //Search for student in database
 while (true) {
-    search = prompt(
-        "Search student records. Type a name[Edwards]. Once your done type 'quit' or press cancel to exit"
-    );
+  search = prompt("Search student records. Type a name[Edwards]. Once your done type 'quit' or press cancel to exit");
 
-    if (search === null || search.toLowerCase() === "quit") {
-        break;
+  if (search === null || search.toLowerCase() === "quit") {
+    break;
+  }
+
+  for (var i = 0; i < students.length; i++) {
+    student = students[i];
+
+    if (student.Name.toLowerCase() === search.toLowerCase()) {
+      message += studentInfo(student);
+      print(message);
     }
-
-    for (var i = 0; i < students.length; i++) {
-        student = students[i];
-
-        if (student.Name.toLowerCase() === search.toLowerCase()) {
-            message += studentInfo(student);
-            print(message);
-        }
-    }
+  }
 }
