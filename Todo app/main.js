8d7a1bcd577473d2e139b5804946ac5e00b178a8
Edwards Presume <edwardspresume@aol.com>
2017-12-15 19:58:46 -0500

    Draft Version 2

--- Todo app/main.js
+++ Todo app/main.js
@@ -1,33 +1,31 @@
-/* DRAFT V1 */
+/* DRAFT V2 */
 
-let todos = ['item1', 'item2', 'item3'];
+let todoList = {
+  //Our list of tasks
+  todos: [
+    'item1', 'item2', 'item3'
+  ],
 
-//Print information to the page
-let print = (msg) => {
-  document.write(msg);
+  //Displays the tasks in our list
+  displayTodos: function() {
+    console.log(this.todos.join(', '));
+  },
+
+  //Add a new task to the list
+  addTodo: function(task) {
+    this.todos.push(task);
+    this.displayTodos();
+  },
+
+  //Change an existing task in our list
+  changeTodo: function(index, newValue) {
+    this.todos[index] = newValue;
+    this.displayTodos();
+  },
+
+  //Delete a task from our list
+  deleteTodo: function(index) {
+    this.todos.splice(index, 1);
+    this.displayTodos();
+  }
 };
-
-//Displays the task in our list
-let displayTodos = () => {
-  print(todos.join(', '));
-};
-
-//Add a new task to the list
-let addTodo = (task) => {
-  todos.push(task);
-  displayTodos();
-};
-
-//Change an existing task in our list
-let changeTodo = (index, newValue) => {
-  todos[index] = newValue;
-  displayTodos();
-};
-
-//Delete a task from our list
-let deleteTodo = (index) => {
-  todos.splice(index, 1);
-  displayTodos();
-};
-
-deleteTodo(2);
