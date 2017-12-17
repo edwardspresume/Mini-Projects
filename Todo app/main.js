44618d890cb952e93329607bf084334facfead9c
Edwards Presume <edwardspresume@aol.com>
2017-12-17 18:40:41 -0500

    Version 4

--- Todo app/main.js
+++ Todo app/main.js
@@ -1,36 +1,32 @@
-/* DRAFT V2 */
+/* DRAFT V4 */
 
 let todoList = {
   //Our list of tasks
-  todos: [
-    'item1', 'item2', 'item3'
-  ],
+  tasks: [],
 
   //Print out information
   printInfo: msg => document.write(msg),
 
   //Displays the tasks in our list
   displayTasks: function() {
-    this.printInfo(this.todos.join(', '));
+    this.printInfo(this.tasks.join(', '));
   },
 
   //Add a new task to the list
   addTask: function(task) {
-    this.todos.push(task);
+    this.tasks.push({task: task, completed: false});
     this.displayTasks();
   },
 
   //Change an existing task in our list
   changeTask: function(index, newValue) {
-    this.todos[index] = newValue;
+    this.tasks[index] = newValue;
     this.displayTasks();
   },
 
   //Delete a task from our list
   deleteTask: function(index) {
-    this.todos.splice(index, 1);
+    this.tasks.splice(index, 1);
     this.displayTasks();
   }
 };
-
-todoList.displayTasks();
