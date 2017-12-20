96f6db78a6d4788f87bba0267a16b01ec2867c50
Edwards Presume <edwardspresume@aol.com>
2017-12-19 19:07:26 -0500

    Added a toggleAll()

--- Todo app/main.js
+++ Todo app/main.js
@@ -1,4 +1,4 @@
-/* DRAFT V4 */
+/* DRAFT V6 */
 
 let todoList = {
   //Our list of tasks
@@ -38,10 +38,36 @@ let todoList = {
     this.displayTasks();
   },
 
-  //Toggles between whether our not the task is completed
+  //Toggles between whether our not a task is completed
   toggleCompleted: function(index) {
     let task = this.tasks[index];
     task.completed = !task.completed;
+    this.displayTasks();
+  },
+
+  //Toggles all of our tasks
+  toggleAll: function() {
+    let totalTaks = this.tasks.length;
+    let completedTasks = 0;
+
+    //Get number of completed tasks.
+    for (let i = 0; i < totalTaks; i++) {
+      if (this.tasks[i].completed) {
+        completedTasks++;
+      }
+    }
+
+    //If everything's true, making everything false.
+    if (completedTasks === totalTaks) {
+      for (let i = 0; i < totalTaks; i++) {
+        this.tasks[i].completed = false;
+      }
+      //Otherwise, make everything true.
+    } else {
+      for (let i = 0; i < totalTaks; i++) {
+        this.tasks[i].completed = true;
+      }
+    }
     this.displayTasks();
   }
 };
