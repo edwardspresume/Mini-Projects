0877dbc6b737264175376860e11b350c6cc78f60
Edwards Presume <edwardspresume@aol.com>
2018-01-30 00:27:49 -0500

    Minnor variable and syntax modification

--- Array-Search/script.js
+++ Array-Search/script.js
@@ -1,21 +1,29 @@
-//An array of our store items
-let storeItems = ['water', 'Blue Berries', 'Black berries', 'Celery', 'Spinach'];
+// An array of our store items
+const storeItems = [
+  "water",
+  "Blue Berries",
+  "Black berries",
+  "Celery",
+  "Spinach"
+];
 
-//Print function
-let print = msg => {
-  let output = document.getElementById('output');
+// Print function
+const print = msg => {
+  const output = document.getElementById("output");
   output.innerHTML = `<h1> ${msg} </h1>`;
 };
 
-//Loops through our array and displays the requested information
+// Loops through our array and displays the requested information
 while (true) {
-  let search = prompt("Search up an item in our store. Type 'list' to display all of our current items. Type 'quit' to exit once your done.");
+  let search = prompt(
+    "Search up an item in our store. Type 'list' to display all of our current items. Type 'quit' to exit once your done."
+  );
   search = search.toLowerCase();
 
-  if (search === 'quit') {
+  if (search === "quit") {
     break;
-  } else if (search === 'list') {
-    print(storeItems.join(', '));
+  } else if (search === "list") {
+    print(storeItems.join(", "));
   } else {
     if (storeItems.indexOf(search) > -1) {
       print(`Yes we have ${search} in our store`);
